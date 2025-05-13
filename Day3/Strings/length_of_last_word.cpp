86c749228dc08594ba6df45c8a5e7e35ba787a6c
blazePrakhar <prakharrajsingh10@gmail.com>
2025-05-14 00:33:55 +0530

    Update length_of_last_word.cpp

--- Day3/Strings/length_of_last_word.cpp
+++ Day3/Strings/length_of_last_word.cpp
@@ -10,12 +10,10 @@ public:
     int lengthOfLastWord(string s) {
         int i = s.length() - 1, len = 0;
 
-        // Skip trailing spaces
         while (i >= 0 && s[i] == ' ') {
             i--;
         }
 
-        // Count the length of the last word
         while (i >= 0 && s[i] != ' ') {
             len++;
             i--;
